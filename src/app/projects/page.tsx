aed8b84689e3243b31e517ff5dfb9a91c0ae5d6f
Wido Do <widodo@saas-studios.com>
2025-02-02 21:24:39 +0700

    push

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -71,7 +71,7 @@ export default function ProjectPage() {
   return (
     <main className="max-w-3xl mx-auto px-4 sm:px-6 md:px-4 py-6 md:py-4 pt-4 md:pt-4 bg-white dark:bg-neutral-900 text-black dark:text-white">
       <h1 className="text-xl font-bold mb-1">Projects</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 relative z-10 py-4 max-w-7xl mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 relative z-0 py-4 max-w-7xl mx-auto">
         {features.map((feature, index) => (
           <Feature key={feature.id} {...feature} index={index} />
         ))}
@@ -108,10 +108,10 @@ const Feature = ({
       ) : (
         <div className="opacity-0 group-hover/feature:opacity-100 transition duration-200 absolute inset-0 h-full w-full bg-gradient-to-b from-neutral-100 dark:from-neutral-800 to-transparent pointer-events-none" />
       )}
-      <div className="mb-4 relative z-10 px-10 text-neutral-600 dark:text-neutral-400">
+      <div className="mb-4 relative z-0 px-10 text-neutral-600 dark:text-neutral-400">
         {icon}
       </div>
-      <div className="text-lg font-bold mb-2 relative z-10 px-10">
+      <div className="text-lg font-bold mb-2 relative z-0 px-10">
         <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-blue-500 transition-all duration-200 origin-center" />
         <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-neutral-800 dark:text-neutral-100">
           {title}
